ad106a490aee1d5475aadb5d1884474199729e6e
YetAnotherMinion <yetanotherminion@gmail.com>
2015-09-13 10:04:39 -0400

    now trying to build petsc

--- install_petsc.sh
+++ install_petsc.sh
@@ -8,7 +8,7 @@
 
 ./configure --prefix=$1 \
   PETSC_ARCH=linux-mpich  PETSC_DIR=$2 \
-  --with-cc=mpicc --with-cxx=mpicxx --with-fc=mpif90 --CXX=clang++ --CC=clang \
+  --with-cc=mpicc --with-cxx=mpicxx --with-fc=mpif90 --CXX=$CXX --CC=$CC \
   --download-fblaslapack=$3/fblaslapack-3.4.2.tar.gz \
   --download-superlu=$3/superlu_4.3.tar.gz \
   --with-pthread=yes
