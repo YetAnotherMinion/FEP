2d2cff659bc16be5f3d1978d78e8663e24d71c38
YetAnotherMinion <yetanotherminion@gmail.com>
2015-09-14 16:50:30 -0400

    actually adding custom build options

---
+++ custom_pumi_configure.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env sh
+
+#first arg is installation directory
+#second arg is test mesh location directory
+
+cmake .. \
+  -DCMAKE_C_COMPILER="mpicc" \
+  -DCMAKE_CXX_COMPILER="mpicxx" \
+  -DCMAKE_C_FLAGS="-cc=$CC -O2 -g -Wall" \
+  -DCMAKE_CXX_FLAGS="-cxx=$CXX -O2 -g -Wall" \
+  -DENABLE_THREADS=OFF \
+  -DENABLE_ZOLTAN=OFF \
+  -DCMAKE_INSTALL_PREFIX="$1" \
+  -DIS_TESTING=True \
+  -DMESHES="$2"
