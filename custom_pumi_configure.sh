18e75e19724f0afbcdcb8bfeead6ace3c268b8e0
YetAnotherMinion <yetanotherminion@gmail.com>
2015-09-14 17:20:55 -0400

    removing compiler arguments since we set them using env varaibles

--- custom_pumi_configure.sh
+++ custom_pumi_configure.sh
@@ -8,12 +8,11 @@ case $MPI in
 
 esac
 
-
 cmake .. \
   -DCMAKE_C_COMPILER="mpicc" \
   -DCMAKE_CXX_COMPILER="mpicxx" \
-  -DCMAKE_C_FLAGS="-cc=$CC -O2 -g -Wall" \
-  -DCMAKE_CXX_FLAGS="-cxx=$CXX -O2 -g -Wall" \
+  -DCMAKE_C_FLAGS="-O2 -g -Wall" \
+  -DCMAKE_CXX_FLAGS="-O2 -g -Wall" \
   -DENABLE_THREADS=OFF \
   -DENABLE_ZOLTAN=OFF \
   -DCMAKE_INSTALL_PREFIX="$1" \
