3de5934c71c8f326d08dea5b7f75d1aea8339d02
YetAnotherMinion <yetanotherminion@gmail.com>
2015-11-06 20:25:01 -0500

    corrected build for new PUMI library with includes -lmth and -llion

--- a4/config.mk
+++ a4/config.mk
@@ -1,11 +1,25 @@
-MPIFLAGS+="-cxx=clang++ -Wno-c++11-long-long"
+CC=gcc
+
+ifeq ($(CC), gcc)
+	MPIFLAGS+=--std=c++0x -Wno-long-long
+else
+	MPIFLAGS+=-Wno-c++11-long-long
+endif
+
+# PETSC_DIR=/tmp/packages/petsc-3.6.1
+# LDIR=$(MAIN_DIR)/lib
+# PUMI_DIR=$(LDIR)/pumi
+# GTEST_DIR=/tmp/googletest
+
+
+# MPIFLAGS+="-cxx=clang++ -Wno-c++11-long-long"
 
 PETSC_DIR = /home/shivaebola/Documents/Software/petsc/petsc-3.6.1
 
-LDIR = /home/shivaebola/Documents/GitHub/FEP/lib
+LDIR = /home/shivaebola/GitLab/FEP/lib
 XERCES_DIR = $(LDIR)/xerces-c-3.1.2/build
 PUMI_DIR = $(LDIR)/pumi
 # Points to the root of Google Test, relative to where this file is.
 # Remember to tweak this if you move this file.
-GTEST_DIR =  ~/Documents/GitHub/googletest
+GTEST_DIR =  ~/GitLab/googletest
 
--- a4/src/ForceContributor2D.cc
+++ a4/src/ForceContributor2D.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <apfMesh.h>
 
 #include "ForceContributor2D.h"
