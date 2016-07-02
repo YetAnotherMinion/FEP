4bd41095878cfaa0b092d3e785793f284f69d9f8
YetAnotherMinion <yetanotherminion@gmail.com>
2016-07-01 20:05:28 -0700

    add debugging prints to see why travis build fails

--- install_petsc.sh
+++ install_petsc.sh
@@ -6,9 +6,17 @@
 # third is full path to directory where packages have been downloaded without
 # trailing slash
 
+echo $3
+ls -al $3
+
 ./configure --prefix=$1 \
   PETSC_ARCH=linux-mpich  PETSC_DIR=$2 \
   --with-cc=mpicc --with-cxx=mpicxx --with-fc=mpif90 --CXX=$CXX --CC=$CC \
   --download-fblaslapack=$3/fblaslapack-3.4.2.tar.gz \
   --download-superlu=$3/superlu_4.3.tar.gz \
   --with-pthread=yes
+
+if [ $? -ne 0 ]; then
+  ls -al
+  cat configure.log;
+fi 
